267896743850eb490fab1eb82025031915a9ce6c
Andrei Muzarau <95071115+amuzarau@users.noreply.github.com>
2025-05-28 23:50:23 +0300

    Add files via upload

---
+++ 27_Fish_Tank.py
@@ -0,0 +1,83 @@
+import random, sys, time
+
+try:
+    import bext
+except ImportError:
+    print('This program requires the bext module, which you')
+    print('can install by following the instructions at')
+    print()
+    sys.exit()
+
+# Set up the constants:
+WIDTH, HEIGHT = bext.size()
+WIDTH -= 1
+
+NUM_KELP = 2
+NUM_FISH = 10
+NUM_BUBBLERS = 1
+FRAMES_PER_SECOND = 4
+
+FISH_TYPES:[
+ {'right': ['><>'],          'left': ['<><']},
+  {'right': ['>||>'],         'left': ['<||<']},
+  {'right': ['>))>'],         'left': ['<[[<']},
+  {'right': ['>||o', '>||.'], 'left': ['o||<', '.||<']},
+  {'right': ['>))o', '>)).'], 'left': ['o[[<', '.[[<']},
+  {'right': ['>-==>'],        'left': ['<==-<']},
+  {'right': [r'>\\>'],        'left': ['<//<']},
+  {'right': ['><)))*>'],      'left': ['<*(((><']},
+  {'right': ['}-[[[*>'],      'left': ['<*]]]-{']},
+  {'right': [']-<)))b>'],     'left': ['<d(((>-[']},
+  {'right': ['><XXX*>'],      'left': ['<*XXX><']},
+  {'right': ['_.-._.-^=>', '.-._.-.^=>',
+             '-._.-._^=>', '._.-._.^=>'],
+   'left':  ['<=^-._.-._', '<=^.-._.-.',
+             '<=^_.-._.-', '<=^._.-._.']},   
+]
+LONGEST_FISH_LENGTH = 10
+
+# The x and y positions where a fish runs into the edge of the screen:
+LEFT_EDGE = 0
+RIGHT_EDGE = WIDTH - 1 - LONGEST_FISH_LENGTH
+TOP_EDGE = 0
+BOTTOM_EDGE = HEIGHT - 2
+
+def main():
+    global FISHES, BUBBLERS, BUBBLES, KELPS, STEP
+    bext.bg('black')
+    bext.clear()
+
+    # Generate the global variables:
+    FISHES = []
+    for i in range(NUM_FISH):
+        FISHES.append(generate_fish())
+
+    # NOTE: Bubbles are drawn, but not the bubblers themselves.
+    BUBBLERS = []
+    for i in range(NUM_BUBBLERS):
+        BUBBLERS.append(random.randint(LEFT_EDGE, RIGHT_EDGE))
+    BUBBLES = []
+
+    KELPS = []
+    for i in range(NUM_KELP):
+        kelpx = random.randint(LEFT_EDGE, RIGHT_EDGE)
+        kelp = {'x': kelpx, 'segments': []}
+        # Generate each segment of the kelp:
+        for i in range(random.randint(6, HEIGHT - 1)):
+            kelp['segments'].append(random.choice(['(', ')']))
+        KELPS.append(kelp)
+
+    # Run the simulation:
+    STEP = 1
+    while True:
+        simulate_aquarium()
+        draw_aquarium()
+        time.sleep(1 / FRAMES_PER_SECOND)
+        clear_aquarium()
+        STEP += 1
+
+def get_random_color():
+    '''Return a string of a random color.'''
+    fish_type = random.choice(FISH_TYPES)
+
+    
